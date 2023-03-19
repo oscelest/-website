4e38cfe4a15b1ac77ef377fc1924c07bd5d8515e
oscelest <oscelest@gmail.com>
2023-03-19 23:42:51 +0100

    Adding API project.

--- noxy.io/components/Game/Dialog/CreateGuildDialog.tsx
+++ noxy.io/components/Game/Dialog/CreateGuildDialog.tsx
@@ -1,8 +1,9 @@
 import {Button} from "@noxy/react-button";
+import {DialogContext} from "@noxy/react-dialog";
 import {InputField, InputFieldChangeEvent} from "@noxy/react-input-field";
 import {useSubscription} from "@noxy/react-subscription-hook";
 import {HTMLComponentProps, sanitizeClassName} from "@noxy/react-utils";
-import React, {useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {subscriptionSocket} from "../../../Globals";
 import Style from "./CreateGuildDialog.module.scss";
 
@@ -10,9 +11,18 @@ export const CreateGuildDialog = (props: CreateGuildDialogProps) => {
   const {className, children, ...component_props} = props;
   const classes = sanitizeClassName(Style.Component, className);
   
+  const context = useContext(DialogContext);
   const [socket] = useSubscription(subscriptionSocket);
   const [value, setValue] = useState<string>("");
-
+  
+  useEffect(
+    () => {
+      socket?.on("CreateGuild", onGuildCreate);
+      return () => socket?.off("CreateGuild", onGuildCreate);
+    },
+    []
+  );
+  
   return (
     <div {...component_props} className={classes}>
       <div className={Style.Header}>Create your Guild</div>
@@ -26,9 +36,14 @@ export const CreateGuildDialog = (props: CreateGuildDialogProps) => {
   function onClick() {
     socket?.send("CreateGuild", value);
   }
-
+  
   function onInputChange(event: InputFieldChangeEvent) {
     setValue(event.value);
+  }
+  
+  function onGuildCreate(guild: object) {
+    console.log(guild);
+    context.close();
   }
   
 };
